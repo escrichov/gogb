41f8dfd782f2e6f994640fad48c1b6c2d687f315
Carlos Escriche Izquierdo <escrichov@gmail.com>
2022-11-08 17:27:26 +0100

    * Fix timers * Unused bits in io set to 1

--- emulator/memory.go
+++ emulator/memory.go
@@ -196,19 +196,57 @@ func (e *Emulator) mem8(addr uint16, val uint8, write bool) uint8 {
 		if addr >= 0xFE00 {
 			if write {
 				switch addr {
-				case 0xFF04:
+				case 0xFF02: // SC: Serial transfer control
+					val |= 0b01111110 // Unused bits returns 1s
+				case 0xFF04: // DIV
 					val = 0
-					e.timaTimer = 0
-				case 0xFF05:
-					e.divTimer = uint16(val)
+					e.divTimer = 0
+				case 0xFF05: // TIMA
+					e.timaTimer = int(val)
+				case 0xFF07: // TAC: Timer Control
+					val |= 0b11111000 // Unused bits returns 1s
+				case 0xFF0F: // IF
+					val |= 0b11100000 // Unused bits returns 1s
+				case 0xFF10: // Audio NR10: Channel 1 sweep
+					val |= 0b10000000 // Unused bits returns 1s
+				case 0xFF1C: // NR32: Channel 3 output level
+					val |= 0b10011111 // Unused bits returns 1s
+				case 0xFF1A: // Audio NR30: Channel 3 DAC enable
+					val |= 0b01111111 // Unused bits returns 1s
+				case 0xFF20: // NR41: Channel 4 length timer [write-only]
+					val |= 0b11000000 // Unused bits returns 1s
+				case 0xFF23: // NR44: Channel 4 control
+					val |= 0b00111111 // Unused bits returns 1s
+				case 0xFF26: // NR52: Sound on/off
+					val |= 0b01110000 // Unused bits returns 1s
+				case 0xFF40: // LCDC: LCD control
+					e.SetLCDC(val)
+				case 0xFF41: // STAT: LCD status
+					val |= 0b10000000 // Unused bits returns 1s
 				case 0xFF46:
 					for y := WIDTH - 1; y >= 0; y-- {
 						e.io[y] = e.read8(uint16(val)<<8 | uint16(y))
 					}
-				case 0xFF40:
-					e.SetLCDC(val)
 				case 0xFF50:
 					e.bootRomEnabled = false
+					val |= 0b11111111 // Unused bits returns 1s
+				case 0xFF03, 0xFF08, 0xFF09, 0xFF0A, 0xFF0B,
+					0xFF0C, 0xFF0D, 0xFF0E, 0xFF15, 0xFF1F,
+					0xFF27, 0xFF28, 0xFF29,
+					0xFF4C, 0xFF4D, 0xFF4E, 0xFF4F,
+					0xFF51, 0xFF52, 0xFF53, 0xFF54,
+					0xFF55, 0xFF56, 0xFF57, 0xFF58,
+					0xFF59, 0xFF5A, 0xFF5B, 0xFF5C,
+					0xFF5D, 0xFF5E, 0xFF5F,
+					0xFF60, 0xFF61, 0xFF62, 0xFF63, 0xFF64,
+					0xFF65, 0xFF66, 0xFF67, 0xFF68,
+					0xFF69, 0xFF6A, 0xFF6B, 0xFF6C,
+					0xFF6D, 0xFF6E, 0xFF6F,
+					0xFF70, 0xFF71, 0xFF72, 0xFF73, 0xFF74,
+					0xFF75, 0xFF76, 0xFF77, 0xFF78,
+					0xFF79, 0xFF7A, 0xFF7B, 0xFF7C,
+					0xFF7D, 0xFF7E, 0xFF7F: // Unused
+					val |= 0b11111111 // Unused bits returns 1s
 				}
 				ioAddr := addr & 0x1ff
 				e.io[ioAddr] = val
